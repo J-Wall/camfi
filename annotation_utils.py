c563bfeb79d5c93860a18e694f3829cdcc03f5cd
Jesse Wallace <jesse.rudolf.wallace@gmail.com>
2021-04-27 12:56:23 +1000

    Added merging and zero-annotation-removal to annotation_utils.py

--- annotation_utils.py
+++ annotation_utils.py
@@ -36,19 +36,43 @@ def _get_metadata_with_key(img_tup):
 
 
 class AnnotationUtils(object):
-    def __init__(self, processes=1):
+    def __init__(self, processes=1, i=None, o=None):
         """
         Parameters
         ----------
         processes : int
             number of child processes to spawn
+        o: str
+            output file. Defaults to sys.stdout
         """
         self.processes = processes
+        self.i = i
+        self.o = o
+
+    def _output(self, out_str):
+        if self.o is None:
+            print(out_str)
+        else:
+            with open(self.o, "w") as f:
+                print(out_str, file=f)
+
+    def _load(self):
+        if self.i is None:
+            annotations = json.load(sys.stdin)
+        else:
+            with open(self.i, "r") as jf:
+                annotations = json.load(jf)
+        return annotations
 
     def add_metadata(self):
+        """
+        Adds image (EXIF) metadata to VIA project.
+
+        Usage: `cat <via_project_file.json> | annotation_utils.py add_metadata > <out>`
+        """
         pool = Pool(self.processes)
 
-        annotation_project = json.load(sys.stdin)
+        annotation_project = self._load()
         file_attributes = set()
 
         img_paths = [
@@ -72,7 +96,41 @@ class AnnotationUtils(object):
                 type="text", description="", default_value=""
             )
 
-        json.dump(annotation_project, sys.stdout, separators=(",", ":"), sort_keys=True)
+        self._output(
+            json.dumps(annotation_project, separators=(",", ":"), sort_keys=True)
+        )
+
+    def remove_unannotated(self):
+        """
+        Removes image metadata from VIA project file for images which have no
+        annotations.
+        """
+        annotation_project = self._load()
+        new_metadata = {}
+
+        for key, img_data in annotation_project["_via_img_metadata"].items():
+            if len(img_data["regions"]) > 0:
+                new_metadata[key] = img_data
+
+        annotation_project["_via_img_metadata"] = new_metadata
+
+        self._output(
+            json.dumps(annotation_project, separators=(",", ":"), sort_keys=True)
+        )
+
+    def merge_annotations(self, *annotation_files):
+        """
+        Takes a list of VIA project files and merges them into one.
+        """
+        with open(annotation_files[0], "r") as jf:
+            annotations = json.load(jf)
+
+        for f in annotation_files[1:]:
+            with open(f, "r") as jf:
+                next_anns = json.load(jf)
+            annotations["_via_img_metadata"].update(next_anns["_via_img_metadata"])
+
+        self._output(json.dumps(annotations, separators=(",", ":"), sort_keys=True))
 
 
 def main():
