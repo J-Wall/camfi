419a792135752ff8faffaa5dc7ad1132ac338c35
Jesse Wallace <jessewacka@hotmail.com>
2021-05-30 13:52:36 +1000

    Type hints for train_model

--- camfi.py
+++ camfi.py
@@ -107,12 +107,12 @@ class UniqueDict(DefaultDict):
         return self[key]
 
 
-class CamfiDataset:
+class CamfiDataset(torch.utils.data.Dataset):
     def __init__(
         self,
-        root: Union[str, bytes, os.PathLike],
+        root: Union[str, os.PathLike],
         transforms: utils.TransformCompose,
-        *via_project_files: Union[str, bytes, os.PathLike],
+        *via_project_files: Union[str, os.PathLike],
         crop: Optional[Tuple[int, int, int, int]] = None,
         point_r: int = 10,
         min_annotations: int = 0,
@@ -393,88 +393,71 @@ def dilate_idx(
 
 
 def train_model(
-    *via_projects,
-    load_pretrained_model=None,
-    img_dir=None,
-    crop=None,
-    point_r=10,
-    mask_dilate=5,
-    min_annotations=0,
-    max_annotations=np.inf,
-    exclude=None,
-    device="cpu",
-    num_classes=2,
-    batch_size=5,
-    num_workers=2,
-    num_epochs=5,
-    outdir=".",
-    model_name=None,
-    save_intermediate=False,
-):
+    *via_projects: Union[str, os.PathLike],
+    load_pretrained_model: Optional[Union[str, os.PathLike]] = None,
+    img_dir: Optional[Union[str, os.PathLike]] = None,
+    crop: Optional[Tuple[int, int, int, int]] = None,
+    point_r: int = 10,
+    mask_dilate: int = 5,
+    min_annotations: int = 0,
+    max_annotations: float = np.inf,
+    exclude: Optional[Union[str, os.PathLike]] = None,
+    device: str = "cpu",
+    num_classes: int = 2,
+    batch_size: int = 5,
+    num_workers: int = 2,
+    num_epochs: int = 5,
+    outdir: Union[str, os.PathLike] = ".",
+    model_name: Optional[str] = None,
+    save_intermediate: bool = False,
+) -> None:
     """
     Parameters
     ----------
-
     via_projects: list of paths
         Path(s) to VIA project files with annotated motion blurs for training on
-
-    load_pretrained_model: str
+    load_pretrained_model: path-like
         Path to model parameters file. If set, will load the pretrained parameters
-
     img_dir
         Path to direcotry containing images. By default inferred from first element in
         via_projects
-
     crop: x0,y0,x1,y1
         Crop images before processing. By default no crop. Original camfi data uses
         --crop=0,0,4608,3312
-
     point_r: int
         Margin added to the coordinates of annotations to determine the bounding box of
         the annotation
-
     mask_dilate: int
         Radius of dilation to apply to training masks
-
     min_annotations: int
         Skip images which have fewer than min_annotations annotations. E.g. to only
         train on images with at least one annotation set `min_annotations=1`
-
-    max_annotations: int
+    max_annotations: float
         Skip images which have more than max_annotations annotations. Set this if you
         are running into memory issues when training on a GPU.
-
-    exclude: str
+    exclude: path-like
         Path to file containing list of images to exclude (one per line). E.g. to
         exclude a test set
-
     device: str
         E.g. "cpu" or "cuda"
-
     num_classes: int
         Number of target classes (including background)
-
     batch_size: int
         Number of images to load at once
-
     num_workers: int
         Number of worker processes for data loader to spawn
-
     num_epochs: int
         Number of epochs to train
-
     outdir: str
         Path to directory where to save model(s)
-
     model_name: str
         Identifier to include in model save file. By default the current date in
         YYYYmmdd format
-
     save_intermediate: bool
         If True, model is saved after each epoch
     """
     # Set params
-    device = torch.device(device)
+    torchdevice = torch.device(device)
     if img_dir is None:
         img_dir = os.path.dirname(via_projects[0])
     if model_name is None:
@@ -497,7 +480,7 @@ def train_model(
         max_annotations=max_annotations,
         exclude=exclude_set,
     )
-    data_loader = torch.utils.data.DataLoader(
+    data_loader: torch.utils.data.DataLoader = torch.utils.data.DataLoader(
         dataset,
         batch_size=batch_size,
         shuffle=True,
@@ -509,7 +492,7 @@ def train_model(
     model = utils.get_model_instance_segmentation(num_classes)
     if load_pretrained_model is not None:
         model.load_state_dict(torch.load(load_pretrained_model))
-    model.to(device)
+    model.to(torchdevice)
 
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
@@ -518,7 +501,7 @@ def train_model(
     for epoch in range(num_epochs):
         # train for one epoch, printing every 10 iterations
         utils.train_one_epoch(
-            model, optimizer, data_loader, device, epoch, print_freq=10
+            model, optimizer, data_loader, torchdevice, epoch, print_freq=10
         )
         # update the learning rate
         lr_scheduler.step()
