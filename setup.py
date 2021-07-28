e79e8b08c75c4e0eca0700aceb1f7cb2c2dc0b18
Jesse Wallace <jesse.rudolf.wallace@gmail.com>
2021-07-29 09:28:10 +1000

    Fixed typo in requirements

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "bcef",
+        "bces",
         "exif",
         "numpy",
         "pandas",
